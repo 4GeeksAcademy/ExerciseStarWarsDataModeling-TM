2ef6de94b44131a74b713726c13ae15bc967414d
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2024-01-25 23:22:32 +0000

    data modeling

--- src/models.py
+++ src/models.py
@@ -7,23 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(35), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_planet = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False)
+    description = Column(String(250))
+
+class Character(Base):
+    __tablename__ = 'character'
+    id_character = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id_favorites = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey('planet.id_planet'))
+    planet = relationship(Planet)
+    character_id = Column(Integer, ForeignKey('character.id_character'))
+    
 
     def to_dict(self):
         return {}
